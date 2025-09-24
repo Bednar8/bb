a83ad49754b73584d194ec33551f3dd38f520956
Paweł Bednarski <pawel.bednarski@meestergroup.pl>
2025-09-24 09:28:45 +0200

    initial structure

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="h-40 bg-blue-300">Footer</footer>;
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="bg-blue-950">Header</header>;
+}
