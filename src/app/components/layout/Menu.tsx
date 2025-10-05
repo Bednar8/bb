7f90cf90cb2a22c0786bedfe1a87ff1d003c4c50
Bednar8 <p.bednarski9907@gmail.com>
2025-10-05 17:22:07 +0200

    add menu component

---
+++ src/app/components/layout/Menu.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import Link from "next/link"
+import {useState} from "react"
+
+const menuItems = [
+	{
+		label: "Portfolio",
+		href: "/projects",
+	},
+	{
+		label: "E-commerce stores",
+		href: "/e-commerce-stores",
+	},
+	{
+		label: "Business websites",
+		href: "/business-websites",
+	},
+	{
+		label: "Our story",
+		href: "/our-story",
+	},
+	{
+		label: "Let's talk",
+		href: "/contact",
+	},
+]
+
+const socialItems = [
+	{
+		label: "Instagram",
+		href: "/instagram",
+	},
+	{
+		label: "TikTok",
+		href: "/tiktok",
+	},
+	{
+		label: "Behance",
+		href: "/behance",
+	},
+	{
+		label: "Insights",
+		href: "/insights",
+	},
+]
+
+export default function Menu() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const toggleMenu = () => {
+		document.body.style.overflow = isMenuOpen ? "auto" : "hidden"
+		setIsMenuOpen(!isMenuOpen)
+	}
+	return (
+		<nav className="flex items-center gap-4">
+			<div className="md:hidden" onClick={toggleMenu}>
+				☰ Burger
+			</div>
+			<div className="hidden md:block space-x-4 uppercase">
+				{menuItems.map((item) => (
+					<Link href={item.href} key={item.label}>
+						{item.label}
+					</Link>
+				))}
+			</div>
+
+			<div
+				className={`md:hidden absolute top-[80px] transition-all duration-300 ${
+					isMenuOpen ? "right-[0%]" : "right-[-110%]"
+				} w-full h-[calc(100vh-80px)] bg-dark text-light-gray flex flex-col justify-between py-10 px-5 uppercase`}>
+				<div className="flex flex-col gap-9">
+					{menuItems.map((item) => (
+						<div
+							key={item.label}
+							className={`menu-link-animate ${isMenuOpen ? "active" : ""}`}>
+							<Link href={item.href}>{item.label}</Link>
+						</div>
+					))}
+				</div>
+				<div className="flex flex-col gap-4 text-xs">
+					{socialItems.map((item) => (
+						<div
+							key={item.label}
+							className={`social-link-animate ${isMenuOpen ? "active" : ""}`}>
+							<Link href={item.href}>{item.label}</Link>
+						</div>
+					))}
+				</div>
+			</div>
+		</nav>
+	)
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,37 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import type {Metadata} from "next"
+import {Geist, Geist_Mono} from "next/font/google"
+import "./globals.css"
+import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
-  title: "BB Development & Design",
-  description: "We create amazing projects with magic ;)",
-};
+	title: "BB Development & Design",
+	description: "We create amazing projects with magic ;)",
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-background text-foreground`}
-      >
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-background text-foreground`}>
+				<Header />
+				<main className="flex-1 mt-[80px]">{children}</main>
+				<Footer />
+			</body>
+		</html>
+	)
 }
