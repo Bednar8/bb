a0b176a10af6990eed661f7358237bab742b54a5
Bednar8 <p.bednarski9907@gmail.com>
2025-10-05 17:27:48 +0200

    fixed linter error

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,39 +1,41 @@
-import { NextRequest, NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server"
 // import nodemailer from "nodemailer";
 
 export async function POST(req: NextRequest) {
-  try {
-    const data = await req.json(); // odczyt danych z frontu
-    console.log("Otrzymane dane:", data);
+	try {
+		const data = await req.json() // odczyt danych z frontu
+		console.log("Otrzymane dane:", data)
 
-    // const transporter = nodemailer.createTransport({
-    //   host: "host",
-    //   port: 465,
-    //   secure: true, // true for 465, false for other ports
-    //   auth: {
-    //     user: "mail",
-    //     pass: "pass",
-    //   },
-    // });
+		// const transporter = nodemailer.createTransport({
+		//   host: "host",
+		//   port: 465,
+		//   secure: true, // true for 465, false for other ports
+		//   auth: {
+		//     user: "mail",
+		//     pass: "pass",
+		//   },
+		// });
 
-    // // Wrap in an async IIFE so we can use await.
-    // await transporter.sendMail({
-    //   from: "Test123 <mail>",
-    //   to: data.email,
-    //   subject: "Temat test",
-    //   text: "Testowy text", // plain‑text body
-    //   html: "<b>Testowy html</b>", // HTML body
-    // });
+		// // Wrap in an async IIFE so we can use await.
+		// await transporter.sendMail({
+		//   from: "Test123 <mail>",
+		//   to: data.email,
+		//   subject: "Temat test",
+		//   text: "Testowy text", // plain‑text body
+		//   html: "<b>Testowy html</b>", // HTML body
+		// });
 
-    return NextResponse.json({
-      status: "success",
-      message: "Formularz wysłany!",
-    });
-  } catch (error: any) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Błąd przy wysyłce maila", error: error.message },
-      { status: 500 }
-    );
-  }
+		return NextResponse.json({
+			status: "success",
+			message: "Formularz wysłany!",
+		})
+	} catch (error: unknown) {
+		console.error(error)
+		const errorMessage =
+			error instanceof Error ? error.message : "Nieznany błąd"
+		return NextResponse.json(
+			{message: "Błąd przy wysyłce maila", error: errorMessage},
+			{status: 500}
+		)
+	}
 }
