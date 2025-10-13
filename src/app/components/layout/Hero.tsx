648bb21c7ad967c2707c27bc531242321847a6d0
Paweł Bednarski <p.bednarski9907@gmail.com>
2025-10-13 11:31:03 +0200

    add spline 3d

---
+++ src/app/components/layout/Hero.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Hero() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/IzsZbTsl6yaz8-6C/scene.splinecode" />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import projects from "@/app/data/projects.json";
 import Link from "next/link";
 import Form from "./components/Form";
+import Hero from "./components/layout/Hero";
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-6xl px-6 py-12">
+      <Hero />
       <section className="mb-16">
         <h2 className="mb-6 text-2xl font-semibold tracking-tight text-neutral-900 dark:text-neutral-100">
           Projects:
