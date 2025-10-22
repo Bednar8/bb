fb5fb18446e9575026b96b633522c403d84c94cc
Paweł Bednarski <p.bednarski9907@gmail.com>
2025-10-22 14:29:24 +0200

    fixed style

--- src/app/components/ui/ProjectStep.tsx
+++ src/app/components/ui/ProjectStep.tsx
@@ -9,13 +9,15 @@ export default function ProjectStep({
 }) {
   return (
     <div
-      className="bg-light-black py-4 px-6 rounded-xl relative"
+      className="bg-light-black py-4 px-6 rounded-xl relative lg:flex lg:flex-col lg:justify-between"
       onMouseEnter={() => onHover(step.id)}
     >
       <p className="relative pl-2.5 text-light-gray  after:content-[''] after:absolute after:left-0 after:top-1/2 after:-translate-y-1/2 after:w-1 after:h-1 after:bg-green-500 after:rounded-full">
         {step.time}
       </p>
-      <h3 className="text-light-gray pt-4 pb-9 text-2xl">{step.title}</h3>
+      <h3 className="text-light-gray pt-4 pb-9 text-2xl lg:pb-3">
+        {step.title}
+      </h3>
       <div
         className="lg:hidden"
         dangerouslySetInnerHTML={{ __html: step.description as string }}
